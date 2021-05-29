9e7981391e67c8fd66857f639a20f7c1d3fbaf07
NidhalDjerbi <nidhaldjerbi@gmail.com>
2021-05-30 00:09:30 +0200

    initial commit

---
+++ app/Models/Product.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'category_id', 
+        'productID',
+        'name',
+        'product_code',
+        'quantity',
+        'price',
+        'brand',
+        'model'
+    ];
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function productCode()
+    {
+        return $this->belongsTo(ProductCode::class);
+    }
+}
---
+++ app/Models/ProductCode.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ProductCode extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'code',
+    ];
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function products()
+    {
+        return $this->hasMany(Product::class);
+    }
+
+}
